12bbcc4022381d6e7a88668e48bf17c803d3b5bc
Correa <ixc3720@yum.com>
2022-01-07 14:29:46 -0600

    Conditionals IF ELSE Fix

--- src/Day_2/BiggestOfThree.java
+++ src/Day_2/BiggestOfThree.java
@@ -8,12 +8,12 @@ public class BiggestOfThree {
 
         if( number1 >= number2 && number1 >= number3){
             System.out.println(number1 + " is the largest number.");
-        }
-        else if (number3 >= number1 && number2 >= number3){
-            System.out.println(number2 + " is the largest number.");
-        }
-        else{
-            System.out.println(number3 + " is the largest number.");
+        }else {
+            if (number3 >= number1 && number2 >= number3) {
+                System.out.println(number2 + " is the largest number.");
+            } else {
+                System.out.println(number3 + " is the largest number.");
+            }
         }
     }
 }
