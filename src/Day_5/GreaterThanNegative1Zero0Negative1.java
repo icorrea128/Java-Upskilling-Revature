eddd8a21a1151d0bd2e8dc04f264657c7a6caf71
Ismael Correa <icorrea81993@gmail.com>
2022-02-03 19:18:45 -0800

    TreeSet Comparable of Different Voyagers on ElectroMagnetic Conductors that travel with pumps of electricty, its just a hypothesis

---
+++ src/Day_5/GreaterThanNegative1Zero0Negative1.java
@@ -0,0 +1,62 @@
+package Day_5;
+
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class GreaterThanNegative1Zero0Negative1 implements Comparable<GreaterThanNegative1Zero0Negative1> {
+
+    String name;
+    int age;
+
+    public GreaterThanNegative1Zero0Negative1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GreaterThanNegative1Zero0Negative1 that = (GreaterThanNegative1Zero0Negative1) o;
+        return age == that.age && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(GreaterThanNegative1Zero0Negative1 o) {
+        if (this.age > o.age) {
+
+            // if current object is greater,then return 1
+            return 1;
+        }
+        else if (this.age < o.age) {
+
+            // if current object is greater,then return -1
+            return -1;
+        }
+        else {
+
+            // if current object is equal to o,then return 0
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Set<GreaterThanNegative1Zero0Negative1> treeSet = new TreeSet<GreaterThanNegative1Zero0Negative1>();
+        GreaterThanNegative1Zero0Negative1 lightyearsOne = new GreaterThanNegative1Zero0Negative1("Voyager",14);
+        GreaterThanNegative1Zero0Negative1 lightyearsTwo = new GreaterThanNegative1Zero0Negative1("Voyager",32);
+        GreaterThanNegative1Zero0Negative1 lightyearsThree = new GreaterThanNegative1Zero0Negative1("Voyager",34);
+        treeSet.add(lightyearsOne);
+        treeSet.add(lightyearsTwo);
+        treeSet.add(lightyearsThree);
+ //I think he wants to be great
+        lightyearsOne.compareTo(lightyearsTwo);
+    }
+}
